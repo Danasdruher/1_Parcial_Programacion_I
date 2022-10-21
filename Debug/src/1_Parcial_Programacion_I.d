ffc77737b20020a1249597d760c7269453570be8
Danasdruher <danielruizher@gmail.com>
2022-10-21 17:56:08 -0300

    primer parcial iniciado

---
+++ Debug/src/1_Parcial_Programacion_I.d
@@ -0,0 +1,1 @@
+src/1_Parcial_Programacion_I.o: ../src/1_Parcial_Programacion_I.c
---
+++ src/1_Parcial_Programacion_I.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : 1_Parcial_Programacion_I.c
+ Author      : Daniel Ruiz
+ Version     :
+ Copyright   : DanielRuiz
+ Description : 1_Parcial_Programacion_I
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+
+
+
+
+	return EXIT_SUCCESS;
+}
